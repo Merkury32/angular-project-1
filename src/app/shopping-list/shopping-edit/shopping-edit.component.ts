04ce5d3831e1caddb555433209ad1a50e66fad2e
Merkury32 <pjozefiak32@gmail.com>
2020-06-15 13:37:19 +0200

    dodałem możliwość usuwania składników z listy zakupów

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -49,9 +49,15 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.editMode = false;
     form.reset();
   }
+
   onClear() {
     this.slForm.reset();
     this.editMode = false;
+  }
+
+  onDelete() {
+    this.slService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
   }
 
   ngOnDestroy() {
