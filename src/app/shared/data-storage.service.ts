d1103414773e36503945e50e9d0e72c84361f5c7
Merkury32 <pjozefiak32@gmail.com>
2020-06-23 10:32:10 +0200

    dodałęm transformowanie danych

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,6 +4,8 @@ import { HttpClient } from "@angular/common/http";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
+import { map } from "rxjs/operators";
+
 @Injectable({ providedIn: "root" })
 export class DataStorageService {
   constructor(private http: HttpClient, private recipeService: RecipeService) {}
@@ -21,6 +23,16 @@ export class DataStorageService {
     this.http
       .get<Recipe[]>(
         "https://angular-project-8cd5e.firebaseio.com/recipes.json"
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
       )
       .subscribe((recipes) => {
         this.recipeService.setRecipes(recipes);
