833ee705d59cbaf8320f5ef054a321ce13b4ce97
Merkury32 <pjozefiak32@gmail.com>
2020-06-25 12:10:00 +0200

    dodałem auto-login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -66,6 +66,30 @@ export class AuthService {
       );
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenEcpirationDate: string;
+    } = JSON.parse(localStorage.getItem("userData"));
+
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenEcpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   logout() {
     this.user.next(null);
   }
@@ -79,6 +103,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem("userData", JSON.stringify(user));
   }
 
   private handlerError(errorRes: HttpErrorResponse) {
