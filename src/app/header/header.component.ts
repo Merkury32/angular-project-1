5ac20b93fc19d69dd9a467c60b81ea20063cb62b
Merkury32 <pjozefiak32@gmail.com>
2020-06-25 10:33:54 +0200

    dodałem token do fetch request

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -18,8 +18,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-    this.onFetchData();
-
     this.userSub = this.authService.user.subscribe((user) => {
       this.isAuthenticated = !!user;
       console.log(!user);
