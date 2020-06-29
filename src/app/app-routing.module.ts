7334badbf14ecccb2dbde4b83a4a1e84a9fb7a40
Merkury32 <pjozefiak32@gmail.com>
2020-06-29 13:12:32 +0200

    dodałem więcej lazy loadingu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,14 @@ const appRoutes: Routes = [
     path: "recipes",
     loadChildren: "./recipes/recipes.module#RecipesModule",
   },
+  {
+    path: "shopping-list",
+    loadChildren: "./shopping-list/shopping-list.module#ShoppingListModule",
+  },
+  {
+    path: "auth",
+    loadChildren: "./auth/auth.module#AuthModule",
+  },
 ];
 
 @NgModule({
