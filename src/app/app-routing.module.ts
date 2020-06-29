85421976e100ed5060d522a75803a803ed51c0a4
Merkury32 <pjozefiak32@gmail.com>
2020-06-29 13:05:54 +0200

    dodałem lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { AuthComponent } from "./auth/auth.component";
 
 const appRoutes: Routes = [
   { path: "", redirectTo: "recipes", pathMatch: "full" },
-  { path: "auth", component: AuthComponent },
+  {
+    path: "recipes",
+    loadChildren: "./recipes/recipes.module#RecipesModule",
+  },
 ];
 
 @NgModule({
