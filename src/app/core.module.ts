be5cf7cb9b33eeb16b43111acc058bfce6ec4248
Merkury32 <pjozefiak32@gmail.com>
2020-06-29 11:07:09 +0200

    dodałem core.module i zmieniłem importy

---
+++ src/app/core.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+
+import { ShoppingListService } from "./shopping-list/shopping-list.service";
+import { RecipeService } from "./recipes/recipe.service";
+import { AuthInceptorService } from "./auth/auth-interceptor.service";
+
+@NgModule({
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInceptorService,
+      multi: true,
+    },
+  ],
+})
+export class CoreModule {}
