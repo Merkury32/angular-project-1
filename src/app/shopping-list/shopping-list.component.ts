36e6696780d27e4cd252e7e2cac03bec56fa6692
Merkury32 <pjozefiak32@gmail.com>
2020-07-01 11:59:31 +0200

    dodałem więcej statów

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -7,6 +7,7 @@ import { LoggingService } from "../logging.service";
 import { Store } from "@ngrx/store";
 
 import * as fromShoppingList from "./store/shopping-list.reducer";
+import * as ShoppingListActions from "./store/shopping-list.actions";
 
 @Component({
   selector: "app-shopping-list",
@@ -38,7 +39,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   onEditItem(index: number) {
-    this.slService.startedEditing.next(index);
+    // this.slService.startedEditing.next(index);
+    this.store.dispatch(new ShoppingListActions.StartEdit(index));
   }
 
   ngOnDestroy(): void {
