f538cf54c5e68ee77120b7df36ad85f5ae946e1e
Merkury32 <pjozefiak32@gmail.com>
2020-07-01 11:02:42 +0200

    dodałem zmienianie i usuwanie rzeczy z listy zakupów

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -3,7 +3,7 @@ import { Ingredient } from "src/app/shared/ingredient.model";
 
 export const ADD_INGREDIENT = "ADD_INGREDIENT";
 export const ADD_INGREDIENTS = "ADD_INGREDIENTS";
-export const UPDATE_INGREDIENTS = "UPDATE_INGREDIENT";
+export const UPDATE_INGREDIENT = "UPDATE_INGREDIENT";
 export const DELETE_INGREDIENT = "DELETE_INGREDIENT";
 
 export class AddIngredient implements Action {
@@ -19,7 +19,7 @@ export class AddIngredients implements Action {
 }
 
 export class UpdateIngredient implements Action {
-  readonly type = UPDATE_INGREDIENTS;
+  readonly type = UPDATE_INGREDIENT;
 
   constructor(public payload: { index: number; ingredient: Ingredient }) {}
 }
