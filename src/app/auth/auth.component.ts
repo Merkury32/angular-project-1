148e632e033a78fb17cc808a1f0044696b4b16ae
Merkury32 <pjozefiak32@gmail.com>
2020-07-03 12:16:57 +0200

    dodałem signup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -61,9 +61,6 @@ export class AuthComponent implements OnInit, OnDestroy {
     const email = form.value.email;
     const password = form.value.password;
 
-    let authObs: Observable<AuthResponseData>;
-
-    this.isLoading = true;
     if (this.isLoginMode) {
       // Obs = this.authService.login(email, password);
       this.store.dispatch(
@@ -73,22 +70,10 @@ export class AuthComponent implements OnInit, OnDestroy {
         })
       );
     } else {
-      authObs = this.authService.signup(email, password);
+      this.store.dispatch(
+        new AuthActions.SignupStart({ email: email, password: password })
+      );
     }
-
-    // authObs.subscribe(
-    //   (resData) => {
-    //     console.log(resData);
-    //     this.isLoading = false;
-    //     this.router.navigate(["/recipes"]);
-    //   },
-    //   (errorMessage) => {
-    //     console.log(errorMessage);
-    //     this.error = errorMessage;
-    //     this.showErrorAlert(errorMessage);
-    //     this.isLoading = false;
-    //   }
-    // );
 
     form.reset();
   }
