69a3cba603c01db1c538b903ff8be7dcf871f4d5
Merkury32 <pjozefiak32@gmail.com>
2020-07-03 14:01:57 +0200

    zainstalowałem ngrx devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HeaderComponent } from "./header/header.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { SharedModule } from "./shared/shared.module";
 import { CoreModule } from "./core.module";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 import { StoreModule } from "@ngrx/store";
 
 import * as fromApp from "./store/app.reducer";
 import { EffectsModule } from "@ngrx/effects";
 import { AuthEffects } from "./auth/store/auth.effects";
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -22,6 +24,7 @@ import { AuthEffects } from "./auth/store/auth.effects";
     AppRoutingModule,
     SharedModule,
     CoreModule,
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
   ],
