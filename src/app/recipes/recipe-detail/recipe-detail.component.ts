77db7eac69f07b2982d6d8a7bd223d20f916427d
Merkury32 <pjozefiak32@gmail.com>
2020-07-06 13:47:20 +0200

    dodałem update, usuwanie i dodawanie receptur

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from "@angular/core";
 import { Recipe } from "../recipe.model";
 import { RecipeService } from "../recipe.service";
 import { ActivatedRoute, Params, Router } from "@angular/router";
-
-import * as fromApp from "../../store/app.reducer";
 import { Store } from "@ngrx/store";
 import { map } from "rxjs/operators";
+
+import * as fromApp from "../../store/app.reducer";
+import * as RecipesActions from "../store/recipe.actions";
 
 @Component({
   selector: "app-recipe-detail",
@@ -53,7 +54,8 @@ export class RecipeDetailComponent implements OnInit {
   }
 
   onDeleteRecipe() {
-    this.recipeService.deleteRecipe(this.id);
+    // this.recipeService.deleteRecipe(this.id);
+    this.store.dispatch(new RecipesActions.DeleteRecipe(this.id));
     this.router.navigate(["/recipes"]);
   }
 }
